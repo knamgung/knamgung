cf415455aa5b71ed4eb30777b348ccc38f2f5aff
Ken N <kngung99@gmail.com>
2022-05-10 16:47:21 -0700

    Fixing css

--- src/components/allProjects.js
+++ src/components/allProjects.js
@@ -206,7 +206,7 @@ export default class AllProjects extends Component {
               active={projectFilter === "CODE"}
               onClick={() => this.setState({ projectFilter: "CODE" })}
             >
-              CODING
+              code
             </FilterButton>
           </FilterButtonDiv>
         </Fade>
